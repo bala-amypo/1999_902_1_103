0cd333f8b6a53bf7e584912a5963abbd4d1ddeb8
bala-amypo <bala@amypo.in>
2025-12-18 15:27:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -1,7 +1,11 @@
 package com.example.demo.entity;
+import java.time.LocalDateTime;
 public class Department{
     private Long id;
     private String name;
     private String description;
-    
+    private String requiredSkills;
+    private LocalDateTime createdAt;
+
+    public int getId()
 }
