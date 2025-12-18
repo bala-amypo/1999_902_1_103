1f58a85cfd54e880c4d3765765be55f352aea8a4
bala-amypo <bala@amypo.in>
2025-12-18 16:06:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -8,4 +8,21 @@ public class Employee{
     private String skills;
     private int maxWeeklyHours;
     private LocalDateTime createdAt;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+    public String getFullName(){
+        return fullname;
+    }
+    public void setFullName(){
+        this.fullname=fullname;
+    }
+    public String getEmail(){
+        return email;
+    }
+    p
 }
