6d04ce608ba12f7303957fe6979f9aef26687885
bala-amypo <bala@amypo.in>
2025-12-18 17:49:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/GeneratedShiftSchedule.java
+++ src/main/java/com/example/demo/entity/GeneratedShiftSchedule.java
@@ -3,10 +3,21 @@ import java.Time.*;
 import jakarta persistence.*;
 public class GeneratedShiftSchedule{
     private Long id;
+    private LocalDate shiftDate;
     private LocalTime startTime;
     private LocalTime endTime;
 
     public int getId(){
-        r
+        return id;
     }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public LocalDate getShiftData(){
+        return shiftDate;
+    }
+    public void setShiftData(LocalDate shiftDate){
+        this.shiftDate=shiftDate;
+    }
+    public 
 }
