d53c9f8da486d9a660ca054b09b6cbc4a8c2147d
bala-amypo <bala@amypo.in>
2025-12-18 17:31:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/EmployeeAvailability.java
+++ src/main/java/com/example/demo/entity/EmployeeAvailability.java
@@ -1,5 +1,10 @@
 package com.example.demo.entity;
+import java.Time.*;
+import jakarta.persistence.*;
 public class EmployeeAvailability{
     private Long id;
-    private 
+    private LocalDate availableData;
+    private Boolean available;
+
+    public int getId()
 }
