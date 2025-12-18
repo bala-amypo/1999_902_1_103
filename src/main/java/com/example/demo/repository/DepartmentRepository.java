8e689147682454cdd4f11f43bc9f270fe77109b2
bala-amypo <bala@amypo.in>
2025-12-18 18:22:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -1,6 +1,11 @@
 package com.example.demo.repository;
 
-import org.springframework.d
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.project.entity.Student;
+
+@Repository
 public interface DepartmentRepository{
     
 }
