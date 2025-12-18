539f7413a500eb81231ac5f68c9e2a5940b9c915
bala-amypo <bala@amypo.in>
2025-12-18 18:01:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/ShiftTemplate.java
+++ src/main/java/com/example/demo/entity/ShiftTemplate.java
@@ -2,5 +2,19 @@ package com.example.demo.entity;
 import java.Time.*;
 import jakarta.persistence.*;
 public class ShiftTemplate{
-    
+    private Long id;
+    private String templateName;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String requiredSkills;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getTemplateName(){
+        return template
+    }
 }
