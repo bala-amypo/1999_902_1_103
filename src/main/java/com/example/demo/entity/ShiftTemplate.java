b9fc4679ebdd6bf6ad81feb8d7389686ba57d4d7
bala-amypo <bala@amypo.in>
2025-12-19 10:34:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/ShiftTemplate.java
+++ src/main/java/com/example/demo/entity/ShiftTemplate.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 @Entity
 public class ShiftTemplate{
-    @ManyToOne
+    @ManyToOneDepartment
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
