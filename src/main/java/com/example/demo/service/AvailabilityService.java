9344caa324f691f03557682f0ac940f4628883c2
bala-amypo <bala@amypo.in>
2025-12-20 17:11:10 +0000

    User commit

--- src/main/java/com/example/demo/service/AvailabilityService.java
+++ src/main/java/com/example/demo/service/AvailabilityService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 public interface AvailabilityService{
     EmployeeAvailability create(EmployeeAvailability availability);
-    EmployeeAvailabilty
+    EmployeeAvailability update(Long id,EmployeeAvailability availability);
+    void delete(Long id)
 }
