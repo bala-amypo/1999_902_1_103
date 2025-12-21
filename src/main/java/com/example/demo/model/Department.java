94e3c31f71c27a46bad31d46d5807177a159a41b
bala-amypo <bala@amypo.in>
2025-12-21 15:36:18 +0000

    User commit

--- src/main/java/com/example/demo/model/Department.java
+++ src/main/java/com/example/demo/model/Department.java
@@ -1,55 +1,56 @@
-package com.example.demo.model;
-import java.time.LocalDateTime;
-import jakarta.persistence.*;
+// package com.example.demo.model;
+// import java.time.LocalDateTime;
+// import jakarta.persistence.*;
 
-@Entity
-public class Department{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String description;
-    private String requiredSkills;
-    private LocalDateTime createdAt;
+// @Entity
+// public class Department{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private Long id;
+//     private String name;
+//     private String description;
+//     private String requiredSkills;
+//     private LocalDateTime createdAt;
 
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getDescription(){
-        return description;
-    }
-    public void setDescription(String description ){
-        this.description=description;
-    }
-    public String getRequiredSkills(){
-        return requiredSkills;
-    }
-    public void setRequiredSkills(String requiredSkills){
-        this.requiredSkills=requiredSkills;
-    }
-    public LocalDateTime getCreatedAt(){
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt){
-        this.createdAt=createdAt;
-    }
-    public Department(Long id,String name,String description,String requiredSkills,LocalDateTime createdAt){
-        this.id=id;
-        this.name=name;
-        this.description=description;
-        this.requiredSkills=requiredSkills;
-        this.createdAt=createdAt;
-    }
-    public Department(){
+//     public Long getId(){
+//         return id;
+//     }
+//     public void setId(Long id){
+//         this.id=id;
+//     }
+//     public String getName(){
+//         return name;
+//     }
+//     public void setName(String name){
+//         this.name=name;
+//     }
+//     public String getDescription(){
+//         return description;
+//     }
+//     public void setDescription(String description ){
+//         this.description=description;
+//     }
+//     public String getRequiredSkills(){
+//         return requiredSkills;
+//     }
+//     public void setRequiredSkills(String requiredSkills){
+//         this.requiredSkills=requiredSkills;
+//     }
+//     public LocalDateTime getCreatedAt(){
+//         return createdAt;
+//     }
+//     public void setCreatedAt(LocalDateTime createdAt){
+//         this.createdAt=createdAt;
+//     }
+//     public Department(Long id,String name,String description,String requiredSkills,LocalDateTime createdAt){
+//         this.id=id;
+//         this.name=name;
+//         this.description=description;
+//         this.requiredSkills=requiredSkills;
+//         this.createdAt=createdAt;
+//     }
+//     public Department(){
 
-    }
-}
+//     }
+// }
+
