e3b54a5b0e90a672ef733bdc14895e25919d8456
bala-amypo <bala@amypo.in>
2025-12-21 15:09:18 +0000

    User commit

--- src/main/java/com/example/demo/service/ScheduleService.java
+++ src/main/java/com/example/demo/service/ScheduleService.java
@@ -1,4 +1,8 @@
 package com.example.demo.service;
+
+import java.util.List;
+
 public interface ScheduleService{
-    
+    List<GeneratedShiftSchedule> generateForDate(LocalDate date);
+    List<GeneratedShiftSchedule> getByDate(LocalDate date);
 }
--- src/main/java/com/example/demo/service/ShiftTemplateService.java
+++ src/main/java/com/example/demo/service/ShiftTemplateService.java
@@ -1,4 +1,5 @@
 package com.example.demo.service;
 public interface ShiftTemplateService{
-    
+    ShiftTemplate create(ShiftTemplate template);
+    List<ShiftTemplate> getByDepartment(Long departmentId);
 }
