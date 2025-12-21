d41cffb46dd0bfc429da85311edfcb1a229ccec5
bala-amypo <bala@amypo.in>
2025-12-21 15:03:18 +0000

    User commit

--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -1,5 +1,10 @@
 package com.example.demo.service;
+
+import java.util.*;
+
 public interface DepartmentService{
     Department create(Department department);
-    Department 
+    Department get(Long id);
+    void delete(Long id);
+    List<Department> getAll();
 }
--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,4 +1,9 @@
 package com.example.demo.service;
+
+import java.time.*;
+
 public interface EmployeeService{
-    
+    Employee createEmployee(Employee employee);
+    Employee getEmployee(Long id);
+
 }
